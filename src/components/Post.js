5892d15894b46c556ff532d8768c28ccc8e00644
dadang6842 <dadang6842@gmail.com>
2024-07-23 11:39:12 +0900

    좋아요 기능 추가 구현 필요

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,19 +1,19 @@
 import { useState } from "react";
-import { useParams, Link, useNavigate } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import axios from 'axios';
 import Comments from './Comments';
 import { useSelector } from "react-redux";
+import postDeleteFunc from "../functions/postDeleteFunc";
 
 function Post({ title, createdAt, username, content, likes, comments }) {
     const [totalLikes, setTotalLikes] = useState(likes);
     const { postId } = useParams();
     const userName = useSelector((state) => state.user);
-    const currentUserName = userName.username;
-    const navigate = useNavigate();
+    const currentUserName = userName.username;  // 좋아요 전송 시 username도 함께 보내야 하는지
 
     const handleLike = async () => {
         try {
-            await axios.post(`/posts/${postId}/like`, {
+            await axios.post(`/posts/${postId}/like`, null, {
                 headers: {
                     Authorization: localStorage.getItem('token'),
                   }
@@ -22,22 +22,9 @@ function Post({ title, createdAt, username, content, likes, comments }) {
         } catch (err) {
             console.error("좋아요 오류:", err);
         }
-    };
+    };  /* 서버에서 좋아요 수를 가져오고 좋아요가 증가할 경우 서버로 데이터를 보내는 추가 로직 작성 필요.
+        이때 username이랑 좋아요 수를 보내면 되는지? */
 
-    const handleDelete = async () => {  // 게시물 삭제
-        try {
-            await fetch(`/posts/${postId}`, {
-                method: 'DELETE',
-                headers: {
-                    Authorization: localStorage.getItem('token')
-                },
-            });
-            alert('게시물이 삭제되었습니다.');
-            navigate("/home");
-        } catch (err) {
-            console.error("게시물 삭제 오류: ", err);
-        }
-    };
 
     return (
         <div>
@@ -47,7 +34,7 @@ function Post({ title, createdAt, username, content, likes, comments }) {
             <Link to={`/posts/${postId}/edit`}>
                 <button>수정</button>
             </Link>
-            <button onClick={handleDelete}>삭제</button>
+            <button onClick={postDeleteFunc}>삭제</button>
             <hr />
             <p>{content}</p>
             <h6 onClick={handleLike}>좋아요 {totalLikes} </h6>
